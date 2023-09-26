f83dee7e64b72cf0e59aba3ca902b4c8b19df35b
ryanlangford07 <144817109+ryanlangford07@users.noreply.github.com>
2023-09-26 14:25:08 +0100

    Add files via upload

---
+++ Flow chart final/Flow chart final/Flow_chart_final.py
@@ -0,0 +1,19 @@
+print("Hello my name is Susan from space")
+print("What is your name")
+
+ 
+
+input1 = input("Enter name: ")
+print(f"hello {input1} I am from the year 2210 and I am 22 years old")
+
+ 
+
+print ("How old are you ?")
+number_1= input("Enter number")
+number_2= 2210
+int(number_1) + int(number_2)
+result= float(number_1) + float(number_2)
+print(f'Wow, by 2210 you will be {result} that is really old')
+print('What music are you in to?')
+input3=input("Enter music type")
+print(f"I have not heard of {input3}")
